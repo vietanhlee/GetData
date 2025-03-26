c36916e50b16516e5a6b74f758bd8192a1bd063c
vietanhlee <levietanhtrump@gmail.com>
2025-03-26 14:28:03 +0700

    up

--- colecting.py
+++ colecting.py
@@ -6,30 +6,55 @@ import pandas as pd
 from PIL import Image, ImageDraw, ImageFont
 import numpy as np
 
+# Gọi tool lấy data points
 tool = TOOL()
 
-# Đọc danh sách từ xấu
-bad_word = pd.read_csv(r"bad_words.csv")
-bad_word = bad_word.values.flatten()
+# Hàm trộn từ (có lưu vào file csv mới)
+def process_combine_words():
+    # Đầu ra là mảng 2D
+    bad_word = pd.read_csv(r"bad_words.csv")
+    good_word = pd.read_csv(r"good_words.csv")
+
+    padding_bad = np.full(shape=(bad_word.shape), fill_value= '1')
+    padding_good = np.full(shape=(good_word.shape), fill_value= '0')
+
+    bad = bad_word.to_numpy() + padding_bad
+    good = good_word.to_numpy() + padding_good
+
+    data = np.concatenate([bad, good])
+
+    # Trộn hoán vị và duỗi thẳng array
+    words_combine = np.random.permutation(data).flatten()
+
+    return words_combine
+
+# Đọc danh sách từ
+data_words = process_combine_words()
 current_index = 0
 
-# Thư mục lưu dữ liệu
-DATA_DIR = r"DATA_TUC"
+# Các biến lính canh
 Is_collecting = True
 saving = False
+
+# Đường dẫn đến font hỗ trợ tiếng Việt
+font_path = "arial.ttf"
+font_size = 30
+font = ImageFont.truetype(font_path, font_size)
 
 # Khởi tạo camera
 cap = cv2.VideoCapture(0)
 cv2.namedWindow('Mouth Points')
 
-# Đường dẫn đến font hỗ trợ tiếng Việt
-font_path = "arial.ttf"  # Thay bằng đường dẫn font của bạn nếu cần
-font_size = 30
-font = ImageFont.truetype(font_path, font_size)
+while Is_collecting and current_index < len(data_words):
+    # Thư mục lưu dữ liệu mặc định
+    DATA_DIR = r"DATA_TUC"
 
-while Is_collecting and current_index < len(bad_word):
-    word = bad_word[current_index]
-    index_word = [f"0{x+1}" for x in range(len(bad_word))]
+    word_origin = data_words[current_index]
+    if(word_origin[-1] == '0'): DATA_DIR = r"DATA_KHONG_TUC"
+
+    word = word_origin[:-1]
+    index_word = [f"0{x+1}" for x in range(len(data_words))]
+
     file_name = os.path.join(DATA_DIR, f'{index_word[current_index]}.csv')
     video_file = os.path.join(DATA_DIR, f'{index_word[current_index]}.avi')
 
@@ -44,13 +69,15 @@ while Is_collecting and current_index < len(bad_word):
 
     with open(file_name, "w", newline="") as file:
         writer = csv.writer(file)
-        word = bad_word[current_index]
+        word = data_words[current_index]
         print(f"Đang chuẩn bị thu thập dữ liệu cho từ: {word}")
 
         while True:
             ret, frame = cap.read()
             if not ret:
                 break
+            
+            frame_cp = frame.copy()
 
             frame = cv2.flip(frame, 1)
             tool.set_input_image(frame)  # Truyền ảnh vào công cụ TOOL
@@ -67,13 +94,13 @@ while Is_collecting and current_index < len(bad_word):
            # Vẽ văn bản lên ảnh chính (frame)
             draw.text((50, 10), "Nhấn S để bắt đầu thu thập dữ liệu",
                       font=font, fill=(255, 0, 0))  # Đỏ
-            draw.text((50, 40), f"Nói: {bad_word[current_index]}",
+            draw.text((50, 40), f"Nói: {word}",
                       font=font, fill=(0, 255, 0))  # Xanh lá
-            draw.text((50, 70), f"Nhấn B để nếu muốn nói lại",
-                      font=font, fill=(0, 255, 0))  # Xanh lá
-            draw.text((50, 100), "Nhấn D để chuyển sang từ tiếp theo",
+            draw.text((50, 70), "Nhấn D để chuyển sang từ tiếp theo",
                       font=font, fill=(0, 0, 255))  # Xanh dương
-
+            draw.text((50, 100), f"Nhấn B để nếu muốn nói lại",
+                      font=font, fill=(0, 255, 0))  # Xanh lá
+            
             # Chuyển lại từ PIL sang OpenCV
             image_with_points = cv2.cvtColor(np.array(pil_image), cv2.COLOR_RGB2BGR)
             bounding_face = tool.draw_bounding_box()
